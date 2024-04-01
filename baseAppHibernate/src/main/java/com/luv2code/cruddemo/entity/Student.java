b6419c4d54e5da20495d357a2485b711becac6cc
IzquierdoDiego <izquierdodiegof@gmail.com>
2024-04-01 11:18:34 -0500

    ID entity generaion type

---
+++ baseAppHibernate/src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -0,0 +1,86 @@
+package com.luv2code.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    // define fields
+    @Id
+    // GenerationType.IDENTITY // Assign primary keys usign database identity column // recommended for mayor of cases
+    // GenerationType.AUTO // Pick an appropiaate stratedy fro the particular database
+    // GenerationType.SEQUENCE // Assign primary keys using a database sequence
+    // GenerationType.TABLE // using underlying database table to ensure uniqueness
+    // for custom generation stately look for org.hibernate.id.IdentifierGenerator
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+    // define constructors
+    public Student() {
+
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // define getters/setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    // define toString() method
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
